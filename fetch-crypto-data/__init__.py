ed4f424abefd287623c39a5291864e7f35672410
bbzlazer <31801714+bbzlazer@users.noreply.github.com>
2021-09-07 18:27:02 -0700

    Add todos and create gitignore
    Add todos for tasks and create a gitignore for future directories

---
+++ fetch-crypto-data/__init__.py
@@ -0,0 +1,4 @@
+# Todo:
+# - Make a function for getting all hourly data for a cryptocurrency
+# - Make a function for getting cryptocurrencies
+# - Make a function to store both in a directory named ./cache, to prevent getting unnecessary data
